b72eaa9069c3f727d084039ff101868be2ca7b37
Chris Titus <dfm.titus@gmail.com>
2019-08-21 14:49:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,9 +9,9 @@ dpkg -l | grep -qw gdebi || sudo apt-get install -yyq gdebi
 sudo apt update
 
 sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-ext-pack \
-net-tools htop grsync lame asunder soundconverter brasero git mc flatpak \
+net-tools htop lame git mc flatpak audacity \
 openssh-server sshfs gedit-plugin-text-size simplescreenrecorder nano \
-ubuntu-restricted-extras mpv vlc gthumb gnome-tweaks tracker chrome-gnome-shell \
+ubuntu-restricted-extras mpv vlc gthumb gnome-tweaks \
 gnome-tweak-tool qt5-style-plugins spell synaptic -yy
 
 # Add me to any groups I might need to be a part of:
@@ -28,7 +28,7 @@ sudo snap remove gnome-characters gnome-calculator gnome-system-monitor
 sudo apt install gnome-characters gnome-calculator gnome-system-monitor \
 gnome-software-plugin-flatpak -yy
 
-# Purge Firefox, install Google Chrome:
+# Purge Firefox, install Chromium:
 
 sudo apt purge firefox -yy
 sudo apt purge firefox-locale-en -yy
@@ -38,13 +38,10 @@ fi
 if [ -d "/home/$USER/.cache/mozilla" ]; then
     rm -rf /home/$USER/.cache/mozilla
 fi
-mkdir /tmp/gc-install-tmp
-pushd /tmp/gc-install-tmp
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo gdebi -n google-chrome-stable_current_amd64.deb
-popd
-rm -rf /tmp/gc-install-tmp
 
+sudo apt install chromium-browser
+
+gsettings set com.ubuntu.update-notifier show-livepatch-status-icon false
 #set icons to minimize on click
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 
@@ -52,17 +49,13 @@ gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
 sudo apt-get remove ubuntu-web-launchers thunderbird rhythmbox -y
 
 ## Multimedia
-sudo apt-get install -y gimp mypaint scribus openshot audacious nautilus-dropbox
+sudo apt-get install -y gimp scribus
 
 ## Games
 sudo apt-get install -y steam
 
 ## Disable Apport
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
-
-# set Qt variable in /etc/environment:
-
-sudo bash -c "echo 'QT_QPA_PLATFORMTHEME=gtk2' >> /etc/environment"
 
 # Gotta reboot now:
 sudo apt update && sudo apt upgrade -y
